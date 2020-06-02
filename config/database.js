a4986b8f575de2ac121110bf84e9f8a229694a3f
micaelgarcez <micaelgarcez@outlook.com>
2020-06-01 22:21:39 -0300

    Adicionando dotenv

--- config/database.js
+++ config/database.js
@@ -1,19 +1,15 @@
+//Trazendo as informaçõe do .env
+require("dotenv").config();
+
 module.exports = {
     dialect: 'mysql',
-    host: 'localhost',
-    port: '3306',
-    username: 'root',
-    password: 'erisintel',
-    database: 'cash',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     define: {
         timestamps: true,
         underscored: true
     }
-    /*,
-    dialectOptions: {
-        useUTC: false, //for reading from database
-        dateStrings: true,
-        typeCast: true
-  },
-  timezone: '-03:00' //for writing to database*/
 };
