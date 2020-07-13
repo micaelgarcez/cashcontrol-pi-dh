16c82afb2d8950744355f1e23025eea20680b965
micaelgarcez <micaelgarcez@outlook.com>
2020-07-13 12:58:20 -0300

    Lading Page Desktop

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,12 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
-mix.sass('src/sass/default.scss', 'public/css')
-   .setPublicPath('public');
-   
-mix.sass('src/sass/all.scss', 'public/css')
-   .setPublicPath('public');
+mix.sass("src/sass/default.scss", "public/css").setPublicPath("public");
+
+mix.sass("src/sass/all.scss", "public/css").setPublicPath("public");
+
+mix
+  .sass("src/sass/LandingPage/landing-page.scss", "public/css")
+  .options({
+    processCssUrls: false,
+  })
+  .setPublicPath("public");
